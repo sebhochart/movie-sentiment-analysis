7fbb6f236be727568ad7daa84dd8f2db454d06ce
Sebastien Hochart <hochart.sebastien@gmail.com>
2023-06-05 16:58:40 -0400

    Added preprocessing to split the script by n words

--- preprocessing/scripts.py
+++ preprocessing/scripts.py
@@ -1,8 +1,10 @@
 import pandas as pd
 from nltk.tokenize import word_tokenize, sent_tokenize
 
-def split_movie_script(movie_file, sentence=True):
-    ''' Gets a movie script as an input, cleans \n and returns a DataFrame of sentences of the script
+def split_movie_script(movie_file, sentence=True, pad=500):
+    ''' Gets a movie script name as an input. Cleans \n chars.
+    If sentence == True, returns a dataFrame of the sentences of the script
+    If sentence = False, returns a dataFrame of chunks of n=pad words from the script (with punctuation, no tokenization, no lemmatization)
     '''
 
     path = './raw_data/screenplay_data/data/raw_texts/raw_texts/'
@@ -14,4 +16,22 @@ def split_movie_script(movie_file, sentence=True):
     movie_script_cleaned = ' '.join(movie_script.split('\n'))
 
     # returning the DataFrame of sentences
-    return pd.DataFrame(sent_tokenize(movie_script_cleaned))
+    if sentence == True:
+        return pd.DataFrame(sent_tokenize(movie_script_cleaned))
+
+    # returning a DataFrame of n=pad words
+    else:
+
+        # split by word (keeping punctuation)
+        movie_script_words = movie_script_cleaned.split(' ')
+
+        # removing empty words
+        while '' in movie_script_words:
+            movie_script_words.remove('')
+
+        # creating the chunks
+        chunks = []
+        for i in range(0, len(movie_script_words), pad):
+            chunks.append(' '.join(movie_script_words[i:i+pad]))
+
+        return pd.DataFrame(chunks)
