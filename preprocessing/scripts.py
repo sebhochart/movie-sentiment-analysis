47d9aa526c38f5e715a30309e94bb8756603bc17
Sebastien Hochart <hochart.sebastien@gmail.com>
2023-06-06 13:31:35 -0400

    Added the get_all_movie_scripts function to get a dict of all preprocessed scripts. Fixed type to chunk_type

--- preprocessing/scripts.py
+++ preprocessing/scripts.py
@@ -2,27 +2,29 @@ import pandas as pd
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
+import os
+import sys
 
-def split_movie_script(movie_file, type='sentence', pad=500, lower=False, rm_stopwords=False, lemmatize=False):
+def split_movie_script(movie_file, chunk_type='sentence', pad=500, lower=False, rm_stopwords=False, lemmatize=False):
     ''' Gets a movie script name as an input. Cleans \n chars.
-    If type == sentence, returns a dataFrame of the sentences of the script
-    If type == words, returns a dataFrame of chunks of pad words from the script (with punctuation, no tokenization, no lemmatization)
+    If chunk_type == sentence, returns a dataFrame of the sentences of the script
+    If chunk_type == words, returns a dataFrame of chunks of pad words from the script (with punctuation, no tokenization, no lemmatization)
 
     If lower == True: all words are lowercased
-    If rm_stopwords == True and sentence == False: stop words are removed
-    If lemmatize == True and sentence == False: text is lemmatized (verbs and nouns)
+    If rm_stopwords == True and type == words: stop words are removed
+    If lemmatize == True and type == words: text is lemmatized (verbs and nouns)
     '''
 
     path = './raw_data/screenplay_data/data/raw_texts/raw_texts/'
 
     # reading the movie script
-    movie_script = open(path + movie_file,'r').read()
+    movie_script = open(path + movie_file, 'r', encoding='iso8859-1').read()
 
     # cleaning \n chars
     movie_script_cleaned = ' '.join(movie_script.split('\n'))
 
     # returning the DataFrame of sentences
-    if type == 'sentence':
+    if chunk_type == 'sentence':
 
         # removing capitalization
         if lower==True:
@@ -31,7 +33,7 @@ def split_movie_script(movie_file, type='sentence', pad=500, lower=False, rm_sto
         return pd.DataFrame(sent_tokenize(movie_script_cleaned))
 
     # returning a DataFrame of n=pad words
-    if type == 'words':
+    if chunk_type == 'words':
 
         # split by word (keeping punctuation)
         movie_script_words = movie_script_cleaned.split(' ')
@@ -60,3 +62,40 @@ def split_movie_script(movie_file, type='sentence', pad=500, lower=False, rm_sto
             chunks.append(' '.join(movie_script_words[i:i+pad]))
 
         return pd.DataFrame(chunks)
+
+
+def get_all_movie_scripts(chunk_type='sentence', pad=500, lower=False, rm_stopwords=False, lemmatize=False):
+    ''' Generates a dictionnary of all movies preprocessed.
+    Same parameters as in the split_movie_script can be applied
+    '''
+
+    # getting the list of file names of all scripts
+    path = './raw_data/screenplay_data/data/raw_texts/raw_texts/'
+    movie_list = os.listdir(path)
+
+    scripts = {}
+
+    for index, movie_file in enumerate(movie_list):
+
+        # printing progress
+        sys.stdout.write('\r')
+        sys.stdout.write(f"{index+1}/{len(movie_list)} movies processed")
+        sys.stdout.flush()
+
+        # split the script in chunks
+        movie_script = split_movie_script(
+            movie_file,
+            chunk_type=chunk_type,
+            pad=pad,
+            lower=lower,
+            rm_stopwords=rm_stopwords,
+            lemmatize=lemmatize
+        )
+
+        # get the movie name and movie id from file name (id is not used but could be later)
+        movie_name, movie_id = movie_file.strip('.txt').split('_')
+
+        # add to the dictionnary
+        scripts[movie_name] = movie_script
+
+    return scripts
