6d9a01df99cf5abf962442365c99452f27d117f1
Sebastien Hochart <hochart.sebastien@gmail.com>
2023-06-06 10:13:17 -0400

    Added parameters to remove capitalization, stop words and lemmmatize. Update with type parameter

--- preprocessing/scripts.py
+++ preprocessing/scripts.py
@@ -1,10 +1,16 @@
 import pandas as pd
 from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
 
-def split_movie_script(movie_file, sentence=True, pad=500):
+def split_movie_script(movie_file, type='sentence', pad=500, lower=False, rm_stopwords=False, lemmatize=False):
     ''' Gets a movie script name as an input. Cleans \n chars.
-    If sentence == True, returns a dataFrame of the sentences of the script
-    If sentence = False, returns a dataFrame of chunks of n=pad words from the script (with punctuation, no tokenization, no lemmatization)
+    If type == sentence, returns a dataFrame of the sentences of the script
+    If type == words, returns a dataFrame of chunks of pad words from the script (with punctuation, no tokenization, no lemmatization)
+
+    If lower == True: all words are lowercased
+    If rm_stopwords == True and sentence == False: stop words are removed
+    If lemmatize == True and sentence == False: text is lemmatized (verbs and nouns)
     '''
 
     path = './raw_data/screenplay_data/data/raw_texts/raw_texts/'
@@ -16,11 +22,16 @@ def split_movie_script(movie_file, sentence=True, pad=500):
     movie_script_cleaned = ' '.join(movie_script.split('\n'))
 
     # returning the DataFrame of sentences
-    if sentence == True:
+    if type == 'sentence':
+
+        # removing capitalization
+        if lower==True:
+            movie_script_cleaned = movie_script_cleaned.lower()
+
         return pd.DataFrame(sent_tokenize(movie_script_cleaned))
 
     # returning a DataFrame of n=pad words
-    else:
+    if type == 'words':
 
         # split by word (keeping punctuation)
         movie_script_words = movie_script_cleaned.split(' ')
@@ -28,6 +39,20 @@ def split_movie_script(movie_file, sentence=True, pad=500):
         # removing empty words
         while '' in movie_script_words:
             movie_script_words.remove('')
+
+        # removing capitalization
+        if lower==True:
+            movie_script_words = [word.lower() for word in movie_script_words]
+
+        # removing stopwords
+        if rm_stopwords == True:
+            stop_words = set(stopwords.words('english'))
+            movie_script_words = [word for word in movie_script_words if not word in stop_words]
+
+        # lemmatize
+        if lemmatize == True:
+            movie_script_words = [WordNetLemmatizer().lemmatize(word, pos='v') for word in movie_script_words]
+            movie_script_words = [WordNetLemmatizer().lemmatize(word, pos='n') for word in movie_script_words]
 
         # creating the chunks
         chunks = []
