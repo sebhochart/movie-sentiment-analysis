e76ca86817fb759c8bdd92e9c5d19582701e81d6
Hossein Poorhemati <hossein.poorhemati@gmail.com>
2023-06-09 01:23:50 -0400

    updatig the structure

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 # setup.py
 from setuptools import setup
+from setuptools import find_packages
+
+# setup(name='movie_sentiment_analysis',
+#       description="Project for analyzing and classifying sentiment arcs from movie scripts",
+#       packages=["movie_sentiment_analysis"]) # You can have several packages, try it
+
+# list dependencies from file
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content]
 
 setup(name='movie_sentiment_analysis',
       description="Project for analyzing and classifying sentiment arcs from movie scripts",
-      packages=["movie_sentiment_analysis"]) # You can have several packages, try it
+      packages=find_packages(), # NEW: find packages automatically
+      install_requires=requirements) # NEW
