be1e3abfe1defc9214302c669745b092d905b95c
Sebastien Hochart <hochart.sebastien@gmail.com>
2023-06-09 14:02:46 -0400

    Mock get_movies_recommandation function

--- movie_sentiment/ml_logic/recommendation.py
+++ movie_sentiment/ml_logic/recommendation.py
@@ -1,4 +1,4 @@
 
 
-def get_movies_recommandation():
+def get_movies_recommendation():
     return ['I Still Know What You Did Last Summer', 'Life of Pi', 'The Accountant', 'The Duchess of Langeais', 'Ghost Ship']
