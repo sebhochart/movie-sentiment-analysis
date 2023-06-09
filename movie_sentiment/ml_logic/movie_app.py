0d25a0930d00ff7ea3fa857b5c4fedfe4982c673
Hossein Poorhemati <hossein.poorhemati@gmail.com>
2023-06-09 04:45:06 -0400

    developing api

--- movie_sentiment/ml_logic/movie_app.py
+++ movie_sentiment/ml_logic/movie_app.py
@@ -1,7 +1,13 @@
 #API
 from fastapi import FastAPI
+from ..processing.arcs import get_all_dyn_arcs
+from ..poster_api import get_poster_url
+
+
 
 app = FastAPI()
+
+arcs = get_all_dyn_arcs()
 
 # Define a root `/` endpoint
 @app.get('/')
@@ -11,6 +17,8 @@ def index():
 
 @app.get('/arc')
 def arc(movie_title):
+    movie_arc = arcs[movie_title]
+    image_url = get_poster_url[movie_title]
+    recom_list =
 
-
-    return {'the arc': 64}
+    return {movie_title: [movie_arc, image_url, recom_list] }
