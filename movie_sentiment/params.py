279db6ecc0283c22213807499ef3f519d58cc028
Sebastien Hochart <hochart.sebastien@gmail.com>
2023-06-09 15:27:12 -0400

    Implemented get_movies_recommendation function

--- movie_sentiment/params.py
+++ movie_sentiment/params.py
@@ -2,12 +2,14 @@
 DICT_PICKLE_FILE = './processed_data/arcs_dict.pickle'
 MIN_LENGHT_ARCS = 30
 
-#dynamic_avg
+# dynamic_avg
 DICT_PICKLE_FILE_DYN_AVG = './processed_data/arcs_dict_dyn_avg.pickle'
 DF_ARCS_PICKLE_FILE_DYN_AVG = './processed_data/df_arcs_dyn_avg.pickle'
 DF_POLY_PICKLE_FILE_DYN_AVG = './processed_data/df_poly_dyn_avg.pickle'
 
-#raw data
+# recommendation cache
+RECO_META_PICKLE_FILE = './processed_data/reco_meta_preprocessed.pickle'
+
+# raw data
 RAW_SCRIPTS_PATH = './raw_data/screenplay_data/data/raw_texts/raw_texts/'
-MIN_LENGHT_ARCS = 30
 METADATA_PATH = './raw_data/movie_metadata/movie_meta_data.csv'
---
+++ movie_sentiment/processing/recommendation.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import numpy as np
+import pickle
+import os
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import TruncatedSVD
+
+from movie_sentiment.processing.arcs import get_all_reshaped_arcs
+from movie_sentiment.params import *
+
+
+def arcs_for_reco(standardize=True):
+    '''Creates a DataFrame of arcs for all movies used in the recommendation algorithm
+    Arcs are standardized if standardize=True
+    '''
+
+    arcs = get_all_reshaped_arcs(dyn_avg=True)
+
+    # standardize the arcs if necessary
+    if standardize == True:
+
+        X = arcs.drop(columns='movie_name')
+
+        s_scaler = StandardScaler()
+        s_scaler.fit(X.T)
+        scaled = s_scaler.transform(X.T)
+        X_scaled = pd.DataFrame(scaled.T, columns=X.columns)
+
+        columns_ids = [i for i in range(30)]
+
+        for i in columns_ids:
+            arcs[i] = X_scaled[i]
+
+    # define movie_name as index
+    df_arcs = arcs.set_index('movie_name')
+
+    return df_arcs
+
+
+def compute_meta_for_reco():
+    '''Creates a DataFrame of metas for all movies used in the recommendation algorithm.
+    Saves in a pickle file if it doesn't exist.
+    '''
+    # read metadata
+    movie_meta = pd.read_csv(METADATA_PATH)
+
+    # keep necessary columns
+    movie_meta = movie_meta[['title', 'keywords', 'genres']]
+
+    # combine keywords and genres columns and clean
+    movie_meta['meta'] = movie_meta['genres'] + ', ' + movie_meta['keywords']
+    movie_meta['meta'] = movie_meta['meta'].str.replace(',', '')
+    movie_meta['meta'] = movie_meta['meta'].fillna('')
+
+    # convert to numerical vectors
+    count = CountVectorizer()
+    count_matrix = count.fit_transform(movie_meta['meta'])
+    count_df = pd.DataFrame(count_matrix.toarray(), index=movie_meta.index.tolist())
+
+    # reduce to 200 dimensions
+    svd = TruncatedSVD(n_components=200)
+    df_meta = svd.fit_transform(count_df)
+
+    # convert to DataFrame
+    df_meta = pd.DataFrame(df_meta, index=movie_meta.title.tolist())
+
+    # Save in pickle file
+    print('Saving in pickle file')
+    with open(RECO_META_PICKLE_FILE, 'wb') as handle:
+        pickle.dump(df_meta, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    print('Saved in pickle file')
+
+    return df_meta
+
+
+def meta_for_reco():
+    '''Loads from pickle file the DataFrame of metas for all movies used in the recommendation algorithm.
+    Generates the data if it doesn't exist.
+    '''
+
+    if os.path.exists(RECO_META_PICKLE_FILE) == True:
+        print('Loading metadata from pickle file')
+        with open(RECO_META_PICKLE_FILE, 'rb') as handle:
+            df_meta = pickle.load(handle)
+
+    else:
+        print('Generating preprocessed meta data for all movies')
+        df_meta = compute_meta_for_reco()
+
+    return df_meta
