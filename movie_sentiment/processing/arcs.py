2e6be8b41ef9281e9bba5c42b2cca3ad59e46ac6
alessandra medeiros de campos <lelemcampos@gmail.com>
2023-06-09 09:15:59 -0300

    new dynamic moving average

--- movie_sentiment/processing/arcs.py
+++ movie_sentiment/processing/arcs.py
@@ -5,7 +5,7 @@ import pandas as pd
 import pickle
 from os.path import exists
 
-from movie_sentiment.ml_logic.movie_score import movie_score
+from movie_sentiment.ml_logic.movie_score import movie_score, movie_score_dyn_avg
 from movie_sentiment.ml_logic.polynomial import script_2_polynomial
 from movie_sentiment.params import *
 from movie_sentiment.processing.reshape_arc import reshaping_arc
@@ -55,6 +55,49 @@ def generate_all_arcs():
 
     return arcs
 
+def generate_all_arcs_new_avg():
+    ''' Generates a dictionnary with the arcs of all movies.
+    Same parameters as in the split_movie_script can be applied
+    '''
+
+    # getting the list of file names of all scripts
+    movie_list = os.listdir(RAW_SCRIPTS_PATH)
+
+    arcs = {}
+
+    for index, movie_file in enumerate(movie_list):
+
+        # printing progress
+        sys.stdout.write('\r')
+        sys.stdout.write(f"{index+1}/{len(movie_list)} movies arcs generated")
+        sys.stdout.flush()
+
+        # get the arc from the movie
+        arc_score = movie_score_dyn_avg(
+            movie_file,
+            chunk_type='sentence',
+            pad=50,
+            group_chunk=10,
+            window_size=50
+        )
+
+        # check if the arc has the minimum required size
+        if len(arc_score) >= MIN_LENGHT_ARCS:
+
+            # get the movie name and movie id from file name (id is not used but could be later)
+            movie_name, movie_id = movie_file.strip('.txt').split('_')
+
+            # add to the dictionnary
+            arcs[movie_name] = arc_score
+
+    # Save in pickle file
+    print('Saving in pickle file')
+    with open(DICT_PICKLE_FILE_DYN_AVG, 'wb') as handle:
+        pickle.dump(arcs, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    print('Saved in pickle file')
+
+    return arcs
 
 def get_all_arcs():
 
@@ -69,13 +112,28 @@ def get_all_arcs():
 
     return arcs
 
+def get_all_dyn_arcs():
 
-def get_all_polynomials():
+    if os.path.exists(DICT_PICKLE_FILE_DYN_AVG) == True:
+        print('Loading data from pickle file')
+        with open(DICT_PICKLE_FILE_DYN_AVG, 'rb') as handle:
+            arcs = pickle.load(handle)
+
+    else:
+        print('Generating data for movie arcs')
+        arcs = generate_all_arcs()
+
+    return arcs
+
+
+def get_all_polynomials(dyn_avg=False):
     '''Creates a DataFrame of polynomial values of movie arcs to feed our model
     Columns of the DataFrame are 'movie_name' and then ids
     '''
-
-    arcs = get_all_arcs()
+    if dyn_avg == False:
+        arcs = get_all_arcs()
+    else:
+        arcs = get_all_dyn_arcs()
     poly = []
 
     # creating a list with the movie name and the coefficients
@@ -91,12 +149,14 @@ def get_all_polynomials():
     # returning the DataFrame
     return pd.DataFrame(poly, columns=columns_id)
 
-def get_all_reshaped_arcs():
+def get_all_reshaped_arcs(dyn_avg=False):
     '''Creates a DataFrame of polynomial values of movie arcs to feed our model
     Columns of the DataFrame are 'movie_name' and then ids
     '''
-
-    arcs = get_all_arcs()
+    if dyn_avg == False:
+        arcs = get_all_arcs()
+    else:
+        arcs = get_all_dyn_arcs()
     reshaped = []
 
     # creating a list with the movie name and the coefficients
