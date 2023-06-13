c726f44d7fb9e4271dca9d3f6d454400617f8080
Hossein Poorhemati <hossein.poorhemati@gmail.com>
2023-06-13 11:58:18 -0400

    improving API and demo page

--- movie_sentiment/ml_logic/movie_app.py
+++ movie_sentiment/ml_logic/movie_app.py
@@ -1,5 +1,6 @@
 #API
 from fastapi import FastAPI
+import pandas as pd
 from ..processing.arcs import get_all_dyn_arcs
 from ..poster_api import get_poster
 from ..ml_logic.recommendation import get_movies_recommendation
@@ -20,16 +21,12 @@ def arc(movie_title, recommendation = True):
 
     if movie_title in ALL_ARCS.keys():
         movie_arc = ALL_ARCS[movie_title]
-
         recom_list = ['set the reccomendation parameter to True to receive recommndations']
         if recommendation:
-
             recom_list = get_movies_recommendation(movie_title=movie_title, n = 5)
 
         response_image = get_poster(movie_title)
-
         classification_info = get_movie_classification(movie_title)
-
 
         return {'arc' : movie_arc,
                 'recom' : recom_list,
@@ -39,3 +36,37 @@ def arc(movie_title, recommendation = True):
     else:
         return {'message' : 'Movie not found in our db 😭, Try another movie!',
                 }
+
+
+@app.get('/recom')
+def recom_df(movie_title):
+
+    recom_list = get_movies_recommendation(movie_title=movie_title, n = 5)
+    arc_list = []
+    poster_list = []
+    score_list = []
+    cluster_list = []
+
+    for movie in recom_list:
+        movie_arc = ALL_ARCS[movie]
+        arc_list.append(movie_arc)
+
+        movie_poster = get_poster(movie)
+        poster_list.append(movie_poster)
+        classification_data = get_movie_classification(movie)
+        movie_score = classification_data[1]
+        score_list.append(movie_score)
+        movie_cluster = classification_data[0]
+        cluster_list.append(movie_cluster)
+
+
+    result = {
+        "Title" : recom_list,
+        "Arc" : arc_list,
+        "Poster" : poster_list,
+        "Score" : score_list,
+        "Cluster" : cluster_list
+        }
+
+
+    return pd.DataFrame(result)
--- movie_sentiment/streamlit/pages/3_Demo.py
+++ movie_sentiment/streamlit/pages/3_Demo.py
@@ -4,45 +4,74 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import time
 from movie_sentiment.processing.arcs import get_all_dyn_arcs
 
 
 st.set_page_config(page_title="Movie Sentiment Analysis - Demo",
                    page_icon="🎥",
                    layout="wide")
+with st.container():
 
-st.title('Movie Sentiment Analysis')
+    with st.container():
 
-movie_titles = sorted(list(get_all_dyn_arcs().keys()))
-select_movie = st.selectbox('Select a movie', movie_titles )
+        st.title('Movie Sentiment Analysis')
 
-# columns = st.columns(3)
+        movie_titles = sorted(list(get_all_dyn_arcs().keys()))
+        select_movie = st.selectbox('Select a movie', movie_titles )
 
-movie_sent_url = 'https://movsentapi-qthbj7hd7q-ew.a.run.app/arc'
+        #API Set up
+        movie_sent_url = 'https://movsentapi-qthbj7hd7q-ew.a.run.app/arc'
+        movie_sent_url_local = 'http://127.0.0.1:8000/arc'
+        params = dict(
+            movie_title=select_movie,
+            recommendation=True
+            )
+        api_reponse = requests.get(movie_sent_url_local, params=params).json()
 
-params = dict(
-    movie_title="Up",
-    recommendation=True
-    )
-api_reponse = requests.get(movie_sent_url, params=params).json()
-
-movie_arc = api_reponse['arc']
-movie_recom = api_reponse['recom']
-movie_poster = api_reponse['image']
-
-st.markdown(movie_arc)
-#plt.plot(movie_arc)
+        # the API response
+        movie_arc = api_reponse['arc']
+        movie_recom = api_reponse['recom']
+        movie_poster = api_reponse['image']
+        movie_score = api_reponse['classificatin_score']
+        movie_cluster = api_reponse['classification_cluster']
 
 
 
+        with st.spinner('Wait for it...'):
+            time.sleep(2.5)
 
-# columns[0].subheader('Select a movie')
-# select_movie = columns[0].selectbox('', movie_titles )
 
-# # select_movie = st.text_input('Select a movie', autocomplete=movie_titles )
 
-# columns[1].markdown('## movie poster')
-# columns[1].markdown('## curve from classification')
+    with st.container():
 
-# columns[2].markdown('## recomendations')
+        col1, col2 = st.columns([1,2.5])
+
+        col1.subheader(select_movie)
+        # poster
+        col1.image(movie_poster)
+
+        col2.subheader('Movie arc')
+        #plotting the arc
+        col2.line_chart(movie_arc)
+
+        # plot info
+        movie_score_str = f"Arc score: {movie_score}"
+        col2.write(movie_score_str)
+        movie_cluster_str = f"This movie has a {movie_cluster} arc."
+        col2.write(movie_cluster_str)
+
+
+    # recommendation container
+    with st.container():
+        st.header(f'Did you like {select_movie}? checkout our recommendations!')
+        # recommendations
+        #API Set up
+        movie_sent_url = 'https://movsentapi-qthbj7hd7q-ew.a.run.app/recom'
+        movie_sent_url_local = 'http://127.0.0.1:8000/recom'
+        params = dict(
+            movie_title=select_movie
+            )
+        api_reponse = requests.get(movie_sent_url_local, params=params).json()
+
+        st.dataframe(api_reponse)
